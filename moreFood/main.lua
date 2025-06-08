369ed299173d753c4cc416fe115577fb809284d2
HTorch <34946917+HTorch@users.noreply.github.com>
2025-06-08 18:26:47 +0800

    Delete moreFood directory

--- moreFood/main.lua
+++
@@ -1,9 +0,0 @@
-moreFood = RegisterMod("moreFood", 1) 
-include("scripts/enums")
-
-if EID then 
-	include("eid/cn/collectibles");
-	include("eid/cn/trinkets");
-	include("eid/en/collectibles_en");
-	include("eid/en/trinkets_en");
-end
